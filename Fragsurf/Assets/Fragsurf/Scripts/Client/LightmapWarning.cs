1ab1e9addefcc92245ceb2e5b18ed8067dd5fad7
Crayz <jacob.ee92@gmail.com>
2021-02-15 08:49:56 -0600

    Better position for lightmap warning

--- Fragsurf/Assets/Fragsurf/Scripts/Client/LightmapWarning.cs
+++ Fragsurf/Assets/Fragsurf/Scripts/Client/LightmapWarning.cs
@@ -7,9 +7,17 @@ namespace Fragsurf.Client
     public class LightmapWarning : FSSharedScript
     {
 
+        private GUIStyle _style;
+
         protected override void _Initialize()
         {
             enabled = false;
+            _style = new GUIStyle()
+            {
+                alignment = TextAnchor.UpperCenter,
+                richText = true,
+                fontSize = 15
+            };
         }
 
         protected override void OnGameLoaded()
@@ -19,8 +27,8 @@ namespace Fragsurf.Client
 
         private void OnGUI()
         {
-            GUI.Label(new Rect(17, 17, 900, 32), "<color=black><size=16>LIGHTMAP NOT BUILT, NOT REPRESENTATIVE OF FINAL QUALITY!</size></color>");
-            GUI.Label(new Rect(16, 16, 900, 32), "<color=red><size=16>LIGHTMAP NOT BUILT, NOT REPRESENTATIVE OF FINAL QUALITY!</size></color>");
+            GUI.Label(new Rect(0, 16, Screen.width, 32), "<color=black><size=16>LIGHTMAP NOT BUILT, NOT REPRESENTATIVE OF FINAL QUALITY!</size></color>", _style);
+            GUI.Label(new Rect(0, 15, Screen.width, 32), "<color=red><size=16>LIGHTMAP NOT BUILT, NOT REPRESENTATIVE OF FINAL QUALITY!</size></color>", _style);
         }
 
     }
