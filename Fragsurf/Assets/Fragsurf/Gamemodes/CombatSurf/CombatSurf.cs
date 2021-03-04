469773be8ae93bf03d87f528d6ba10d05d2f4684
Crayz <jacob.ee92@gmail.com>
2021-03-03 21:50:56 -0600

    Combat surf boilerplate

---
+++ Fragsurf/Assets/Fragsurf/Gamemodes/CombatSurf/CombatSurf.cs
@@ -0,0 +1,18 @@
+using Fragsurf.Shared;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fragsurf.Gamemodes.CombatSurf
+{
+    public class CombatSurf : BaseGamemode
+    {
+        protected override void _Load(FSGameLoop game)
+        {
+        }
+        protected override void _Unload(FSGameLoop game)
+        {
+        }
+    }
+}
+
