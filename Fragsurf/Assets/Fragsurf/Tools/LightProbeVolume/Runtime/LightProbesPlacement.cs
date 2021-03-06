01a4f98a4bf675d6e793c49768da02f3f7622e40
Crayz <jacob.ee92@gmail.com>
2021-03-05 22:40:30 -0600

    LightProbeVolumes

---
+++ Fragsurf/Assets/Fragsurf/Tools/LightProbeVolume/Runtime/LightProbesPlacement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace LightingTools.LightProbesVolumes
+{
+    public static class LightProbesPlacement
+    {
+    }
+}
