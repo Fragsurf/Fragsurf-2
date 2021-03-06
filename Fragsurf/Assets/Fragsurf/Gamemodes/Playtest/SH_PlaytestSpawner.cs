0404cdad1dd7c8a7db3b60d3d647f185a92c25c6
Crayz <jacob.ee92@gmail.com>
2021-03-05 22:12:27 -0600

    Test axe & bat

--- Fragsurf/Assets/Fragsurf/Gamemodes/Playtest/SH_PlaytestSpawner.cs
+++ Fragsurf/Assets/Fragsurf/Gamemodes/Playtest/SH_PlaytestSpawner.cs
@@ -55,7 +55,7 @@ namespace Fragsurf.Gamemodes.Playtest
             player.Entity = ent;
             Game.EntityManager.AddEntity(ent);
 
-            ent.Give("Knife");
+            ent.Give("Axe");
         }
 
     }
