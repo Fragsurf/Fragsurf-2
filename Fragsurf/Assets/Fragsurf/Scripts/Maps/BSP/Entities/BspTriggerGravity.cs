f04f3c7a7296b2cce1a60b942f84d5ea5c04047b
Crayz <jacob.ee92@gmail.com>
2021-03-06 16:24:35 -0600

    Component for trigger_gravity

---
+++ Fragsurf/Assets/Fragsurf/Scripts/Maps/BSP/Entities/BspTriggerGravity.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fragsurf.BSP;
+using Fragsurf.Utility;
+using SourceUtils.ValveBsp.Entities;
+using Fragsurf.Shared.Entity;
+using Fragsurf.Shared.Player;
+
+namespace Fragsurf.BSP
+{
+    [EntityComponent("trigger_gravity")]
+    public class BspTriggerGravity : BspTrigger<TriggerGravity>
+    {
+        protected override void OnStartTouch(NetEntity entity)
+        {
+            if (!(entity is Human hu)
+                || !(hu.MovementController is DefaultMovementController move))
+            {
+                return;
+            }
+            move.MoveData.GravityFactor = Entity.Gravity;
+        }
+    }
+}
+
