f9d9a3692770da611fa2546c0def602b48461c71
Crayz <jacob.ee92@gmail.com>
2021-06-27 11:33:21 -0500

    Mixed subtractive lighting for bsps

--- Assets/Fragsurf/Scripts/Maps/BSP/Entities/BspLightEnvironment.cs
+++ Assets/Fragsurf/Scripts/Maps/BSP/Entities/BspLightEnvironment.cs
@@ -15,6 +15,12 @@ namespace Fragsurf.BSP
             BspToUnity.Options.Sun.transform.localPosition = Vector3.zero;
             BspToUnity.Options.Sun.transform.rotation = Quaternion.Euler(new Vector3(-Entity.Pitch, -Entity.Angles.Y + 90, -Entity.Angles.X));
             BspToUnity.Options.Sun.color = new Color32(Entity.Light.R, Entity.Light.G, Entity.Light.B, Entity.Light.A);
+            //BspToUnity.Options.Sun.lightmapBakeType = LightmapBakeType.Mixed;
+            //BspToUnity.Options.Sun.mix = LightmapBakeType.Mixed;
+
+            //var prop = BspToUnity.Options.Sun.GetType().GetField("isBaked", System.Reflection.BindingFlags.NonPublic
+            //    | System.Reflection.BindingFlags.Instance);
+            //prop.SetValue(BspToUnity.Options.Sun, true);
         }
     }
 }
